1289b00dece310084bbeae31df67a1aecd5d2cf2
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-11-02 08:48:39 -0600

    cleanup

--- my-state.ts
+++ my-state.ts
@@ -1,4 +1,3 @@
-//import { state } from "https://esm.sh/@microsoft/fast-element/state.js";
 import { state } from "@microsoft/fast-element/state.js";
 
 export const myState = state({ name: "World" });
